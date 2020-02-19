94195c83847eef2fcdeb677463f8bd480a99d633
Dotz0cat <47311734+Dotz0cat@users.noreply.github.com>
2020-02-19 16:19:26 -0600

    Update gui.java

--- src/com/Ocat/graphics/gui.java
+++ src/com/Ocat/graphics/gui.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.Ocat.graphics;
 
 import com.Ocat.main.gopher;
@@ -13,10 +8,6 @@ import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author arnoldseth21
- */
 public class gui extends javax.swing.JFrame {
 
     /**
