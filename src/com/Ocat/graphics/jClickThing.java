12d3668b5cace30726f0b82f7cc534914ce8fc45
Dotz0cat <47311734+Dotz0cat@users.noreply.github.com>
2020-02-24 12:43:06 -0600

    Add files via upload

--- src/com/Ocat/graphics/jClickThing.java
+++ src/com/Ocat/graphics/jClickThing.java
@@ -1,17 +1,24 @@
 package com.Ocat.graphics;
 
+import com.Ocat.main.gopherGraphical;
 import javax.swing.JLabel;
 
 public class jClickThing extends JLabel {
     private char type;
     private String selector;
     private String url;
+    private gopherGraphical n;
     
     public jClickThing(char type, String text, String selctor, String url) {
         super(text);
         this.type = type;
         this.selector = selctor;
         this.url = url;
+    }
+    
+    public jClickThing(String text, gopherGraphical gopher) {
+        super(text);
+        this.n = gopher;
     }
     
     public char getType() {
@@ -24,5 +31,9 @@ public class jClickThing extends JLabel {
     
     public String getUrl() {
         return url;
+    }
+    
+    public gopherGraphical getGopher() {
+        return n;
     }
 }
